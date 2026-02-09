be8055a702bcc81ed92509141e4906cb01d41314
Aleksandr <95569587+Aubameyang17@users.noreply.github.com>
2026-02-09 11:41:48 +0300

    Enhance main button click with query_id alert
    Added alert to display query_id when main button is clicked.

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 
 tg.MainButton.setText("SEND");
+
 tg.MainButton.show();
 
 tg.MainButton.onClick(() => {
+alert("query_id = " + tg.initDataUnsafe?.query_id);
+
   tg.sendData("PING_FROM_GITHUB");
 });
